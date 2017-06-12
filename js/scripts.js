a84ce72a2f724f032347813d7f76bdfc6034ae1a
Calla Rudolph and Jacob Ruleaux <calla@>
2017-06-12 16:19:32 -0700

    add prototype for fullAddress

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,9 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 // user interface logic
 
 
@@ -62,7 +65,7 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
 
