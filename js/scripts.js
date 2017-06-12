628bfa29791bc997650bfa843a4e8f8df5bbfb7d
Calla Rudolph and Jacob Ruleaux <jacob@>
2017-06-12 16:30:03 -0700

    create resetFields function for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,14 @@ Contact.prototype.fullName = function() {
 Address.prototype.fullAddress = function() {
   return this.street + ", " + this.city + ", " + this.state;
 }
+
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+}
 // user interface logic
 
 
@@ -69,10 +77,7 @@ $(document).ready(function() {
       });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
+
   });
 });
