e012e3a82cbff3a99b86df51f1e91d50683c907e
Calla Rudolph <callarudolph@gmail.com>
2017-06-11 22:16:32 -0700

    add html, css, js functions to .show-contact

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,13 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
