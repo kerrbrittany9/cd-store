742b9782a0d56db75a1f6bd297c9e99dfb2a20ef
Brittany Kerr and Calla Rudolph <calla@>
2017-06-13 10:32:20 -0700

    select box for address type with id of additional to remove

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ function Contact(first, last) {
   this.addresses = [];
 }
 
-function Address(street, city, state) {
+function Address(type, street, city, state) {
+  this.addressType = type;
   this.street = street;
   this.city = city;
   this.state = state;
@@ -16,15 +17,17 @@ Contact.prototype.fullName = function() {
 }
 
 Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
+  return this.addressType + ": <br>" + this.street + ", " + this.city + ", " + this.state;
 }
 
 function resetFields() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("#addressType").val("Home");
     $("input.new-street").val("");
     $("input.new-city").val("");
     $("input.new-state").val("");
+    $("#additional").remove();
 }
 // user interface logic
 
@@ -32,7 +35,12 @@ function resetFields() {
 $(document).ready(function() {
 
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
+    $("#new-addresses").append('<div class="new-address" id="additional">' +
+                                  '<label for="new-type">Address Type</label>' +
+                                  '<select class="form-control" id="addressType">' +
+                                    '<option value="Home">Home</option>' +
+                                    '<option value="Work">Work</option>' +
+                                  '</select><br>' +
                                  '<div class="form-group">' +
                                    '<label for="new-street">Street</label>' +
                                    '<input type="text" class="form-control new-street">' +
@@ -57,10 +65,11 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-address").each(function() {
+      var inputtedType = $(this).find("#addressType").val();
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState);
       newContact.addresses.push(newAddress);
     });
 
